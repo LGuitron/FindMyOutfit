f6e68eb6c2271a0c0177dbdec0c4b29d4fb72c1f
LGuitron <lguitron1995@gmail.com>
2018-10-13 10:37:49 -0500

    New angular project

--- findOutfitAngular/findMyOutfit/src/app/app.module.ts
+++ findOutfitAngular/findMyOutfit/src/app/app.module.ts
@@ -2,14 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { TituloComponent } from './components/titulo/titulo.component';
-import { ListaSugerenciasComponent } from './components/lista-sugerencias/lista-sugerencias.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    TituloComponent,
-    ListaSugerenciasComponent
+    AppComponent
   ],
   imports: [
     BrowserModule
