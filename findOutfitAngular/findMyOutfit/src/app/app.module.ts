733a09777f758d5a5f432dfc0d35ac503ce480ce
Brandon <angiebrand10@hotmail.com>
2018-10-13 13:11:54 -0500

    Componentes Login, Registro, Titulo con sus scss y html

--- findOutfitAngular/findMyOutfit/src/app/app.module.ts
+++ findOutfitAngular/findMyOutfit/src/app/app.module.ts
@@ -4,12 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { TituloComponent } from './components/titulo/titulo.component';
 import { ListaSugerenciasComponent } from './components/lista-sugerencias/lista-sugerencias.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegistroComponent } from './components/registro/registro.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TituloComponent,
-    ListaSugerenciasComponent
+    ListaSugerenciasComponent,
+    LoginComponent,
+    RegistroComponent
   ],
   imports: [
     BrowserModule
