cf2b9bda565aebe62800fff3211da83b3ac87dee
LGuitron <lguitron1995@gmail.com>
2018-10-13 10:39:23 -0500

    component directory

--- findOutfitAngular/findMyOutfit/src/app/app.module.ts
+++ findOutfitAngular/findMyOutfit/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TituloComponent } from './components/titulo/titulo.component';
+import { ListaSugerenciasComponent } from './components/lista-sugerencias/lista-sugerencias.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TituloComponent,
+    ListaSugerenciasComponent
   ],
   imports: [
     BrowserModule
