abe8c229622de0a70ad28e2596d42e7053dd9e91
Brandon <angiebrand10@hotmail.com>
2018-12-02 04:24:21 -0600

    Login funcional, guarda usuario

--- findOutfitAngular/findMyOutfit/src/app/components/login/login.component.ts
+++ findOutfitAngular/findMyOutfit/src/app/components/login/login.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import {FormGroup, FormControl, Validators} from '@angular/forms';
+import internalApis from '../../../assets/json/internalApis.json';
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+import { map, catchError, tap } from 'rxjs/operators';
+import {Md5} from "md5-typescript";
 
 @Component({
   selector: 'app-login',
@@ -13,7 +17,7 @@ export class LoginComponent implements OnInit {
  	password: new FormControl(null, Validators.required)
   });
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
   }
@@ -23,8 +27,40 @@ export class LoginComponent implements OnInit {
   		console.log('Invalid');
   		return;
   	}
-  	console.log(JSON.stringify(this.loginForm.value));
-  }
+
+  	var newJson = this.loginForm.value;
+
+  	
+  	let request_body = {
+
+             
+								  "email": newJson.email,
+								  "password": newJson.password,
+								
+
+
+                          };
+
+
+  	this.http.post("http://localhost:5000/users/login", request_body).subscribe(response => {
+  			console.log(response);
+  			if(response.status.statusCode == 200){
+  				var user_email = response.user;
+  				console.log(user_email);
+  				localStorage.setItem("user_email", user_email);
+  			}else{
+  				console.log("usuario no encontrado")
+  			}
+  		
+
+  		}, err => {
+      });
+
+
+  
+ }
+
 
 
 }
+
