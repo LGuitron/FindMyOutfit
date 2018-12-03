dc3792c834d284353c30964b1d4146bff758ada2
Elizabeth Rodriguez Fallas <elizabeth@MacBook-Pro-de-Elizabeth.local>
2018-12-03 16:15:19 -0600

    complete animation

--- findOutfitAngular/findMyOutfit/src/app/components/busca-outfit/busca-outfit.component.ts
+++ findOutfitAngular/findMyOutfit/src/app/components/busca-outfit/busca-outfit.component.ts
@@ -3,11 +3,24 @@ import {NavbarComponent} from '../navbar/navbar.component';
 import { ImageTransferService } from '../../services/image-transfer.service';
 import {FormGroup, FormControl, FormBuilder, Validators} from '@angular/forms';
 import {Router} from "@angular/router";
+import {trigger, state,style,animate,transition} from '@angular/animations';
 
 @Component({
   selector: 'app-busca-outfit',
   templateUrl: './busca-outfit.component.html',
-  styleUrls: ['./busca-outfit.component.scss']
+  styleUrls: ['./busca-outfit.component.scss'],
+  animations: [
+    trigger('popOverState',[
+      state('show',style({
+        opacity: 1
+    })),
+    state('hide',style({
+      opacity: 0
+    })),
+    transition('show => hide',animate('600ms ease-out')),
+    transition('hide => show',animate('1000ms ease-in'))
+  ])
+  ]
 })
 export class BuscaOutfitComponent implements OnInit {
 
@@ -16,6 +29,23 @@ export class BuscaOutfitComponent implements OnInit {
   @Output() formReady = new EventEmitter<FormGroup>()
   imageForm = new FormGroup({url : new FormControl()});
 
+   //animations 
+   show = false; 
+   get stateName(){
+     if(!this.show){
+      document.getElementById("botonAnim").textContent="Ayuda";
+     }else{
+      document.getElementById("botonAnim").textContent="OK";
+     }
+     return this.show ? 'show' : 'hide'
+   }
+ 
+   toggle(){
+     this.show = !this.show; 
+    
+
+   }
+ 
 
   constructor(private formBuilder : FormBuilder, public transferService: ImageTransferService, private router: Router)
   {
